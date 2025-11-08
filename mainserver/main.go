03e7a9806326c97cd9c78c859d3d2ceb6f02563b
Matthew Franchini <matchewf11@gmail.com>
2025-11-08 12:35:40 -0800

    init the server

---
+++ mainserver/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"UFinance/api"
+	"UFinance/banking"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // frontend origin(s)
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	banking.BankingRouter(r)
+	api.APIRoutes(r)
+
+	log.Println("Server running on http://localhost:8080")
+	if err := r.Run(":8080"); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Server failed: %v\n", err)
+	}
+}
